83f2b5f833c678587a0d3023c129128ec34a702f
CelestialCrafter <celestialcraftermc@gmail.com>
2024-12-15 10:41:29 -0600

    adapt python chatgpt completions

--- python/chatGPT-completions/demo.py
+++ python/chatGPT-completions/demo.py
@@ -1,57 +1,53 @@
-import os, sys
-
-from groundx import Groundx, ApiException
-import openai
+import os
+import sys
+from groundx import GroundX
+from openai import OpenAI
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-if os.getenv("GROUNDX_API_KEY") is None or os.getenv("OPENAI_API_KEY") is None:
-    raise Exception(
-        """
+if os.getenv("GROUNDX_API_KEY") is None:
+    raise Exception("environment variable GROUNDX_API_KEY is not set")
+if os.getenv("OPENAI_API_KEY") is None:
+    raise Exception("environment variable OPENAI_API_KEY is not set")
 
-    You have not set a required environment variable (GROUNDX_API_KEY or OPENAI_API_KEY)
-    Copy .env.sample and rename it to .env then fill in the missing values
-"""
-    )
+opts = {
+    # Your question
+    "query": "Transformer",
 
-# GroundX Project ID or Bucket ID
-# Found in your GroundX account
-# OR by querying /project, /bucket
-groundxId = 0
+    # GroundX Project ID or Bucket ID
+    # Found in your GroundX account
+    # OR by querying /project, /bucket
+    "id": 13403,
 
-# Your question
-query = "YOUR QUERY HERE"
+    # Instructions sent to ChatGPT for completions
+    # You may want to change this
+    "instruction": "You are a helpful virtual assistant that answers questions using the content below. Your task is to create detailed answers to the questions by combining your understanding of the world with the content provided below. Do not share links.",
 
-# Instructions sent to ChatGPT for completions
-# You may want to change this
-instruction = "You are a helpful virtual assistant that answers questions using the content below. Your task is to create detailed answers to the questions by combining your understanding of the world with the content provided below. Do not share links."
+    # OpenAI model
+    "openai_model": "gpt-4o-mini"
+}
 
 # Initialize the GroundX and OpenAI clients
-groundx = Groundx(
-    api_key=os.getenv("GROUNDX_API_KEY"),
-)
-
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
+groundx = GroundX(api_key=os.getenv("GROUNDX_API_KEY"))
+openai = OpenAI()
 
 # Do a GroundX search
 try:
-    content_response = groundx.search.content(id=groundxId, query=query)
-    results = content_response.body["search"]
-except ApiException as e:
-    print("Exception when calling SearchApi.content: %s\n" % e)
+    content_response = groundx.search.content(id=opts["id"], query=opts["query"])
+    results = content_response.search
+except Exception as e:
+    raise Exception("Exception when calling SearchApi.content: %s\n" % e)
 
 # Access our suggested retrieved context
-llmText = results["text"]
-
+llmText = results.text
 if llmText == "":
     raise Exception("\n\n\tYour search returned an empty result\n")
 
 # Do an OpenAI generation with the retrieved context
 completion = openai.chat.completions.create(
-    model=os.getenv("OPENAI_MODEL"),
+    model=opts["openai_model"],
     messages=[
         {
             "role": "system",
@@ -60,18 +56,18 @@ completion = openai.chat.completions.create(
 %s
 ===
 """
-            % (instruction, llmText),
+            % (opts["instruction"], llmText),
         },
-        {"role": "user", "content": query},
+        {"role": "user", "content": opts["query"]},
     ],
 )
 
 if len(completion.choices) == 0:
-    print("\n\n\tempty result from OpenAI for query [%s]\n\ntresult:\n\n\n" % query)
+    print("\n\n\tempty result from OpenAI for query [%s]\n\ntresult:\n\n\n" % opts["query"])
     print(completion)
     sys.exit()
 
 print(
     "\n\nQUERY\n\n%s\n\n\nSCORE\n\n[%.2f]\n\n\nRESULT\n\n%s\n\n\n"
-    % (query, results["score"], completion.choices[0].message.content)
+    % (opts["query"], results.score, completion.choices[0].message.content)
 )
